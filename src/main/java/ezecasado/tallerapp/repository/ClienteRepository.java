d5664f2da3e28d51a126a485d9b3e2791582c48b
EzeCasado <ezecasado2002@gmail.com>
2026-06-05 23:05:00 -0300

    feat(backend): creacion de GastoVehiculoDTO y logica de gastos totales

--- src/main/java/ezecasado/tallerapp/repository/ClienteRepository.java
+++ src/main/java/ezecasado/tallerapp/repository/ClienteRepository.java
@@ -3,6 +3,7 @@ package ezecasado.tallerapp.repository;
 import ezecasado.tallerapp.models.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,4 +12,6 @@ public interface ClienteRepository extends JpaRepository<Cliente,Long> {
     Optional<Cliente> findByEmail(String email);
 
     List<Cliente> findByActivoTrue();
+
+
 }
